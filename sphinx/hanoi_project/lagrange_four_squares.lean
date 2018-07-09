b8467548feeeeb238104c283abe637e17e30b4ec
Chau Huyen Nguyen <huyenchau19832014@gmail.com>
2018-07-09 10:48:17 +0700

    update lagrange four squares

--- sphinx/hanoi_project/lagrange_four_squares.lean
+++ sphinx/hanoi_project/lagrange_four_squares.lean
@@ -3,6 +3,10 @@
 
 theorem theorem_Lagrange_four_squares (n : ℕ) :
  ∃ a b c d : ℕ,  n = a*a + b*b + c*c + d*d :=
+<<<<<<< HEAD
  begin
  admit, 
  end
+=======
+sorry
+>>>>>>> first two formalizations
